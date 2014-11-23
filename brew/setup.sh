3b5a6c162c1cecf5765decf74cad4421da94d5de
maguhiro <maguhiro@gmail.com>
2014-11-24 00:28:05 +0900

    homebrewの設定

---
+++ brew/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+brew update
+
+brew install zsh
+brew install zsh-completions
+brew install tmux
+brew install git
+
+brew install caskroom/cask/brew-cask
--- setup.sh
+++ setup.sh
@@ -10,7 +10,23 @@ BASE_DIR=$(cd $(dirname $0);pwd)
 cd $BASE_DIR
 
 # リストの分だけ繰り返してシンボリックリンクをはる
+echo "Create: Symbolic Link Dotfile"
 for FILE in $SYMLINK_LIST; do
   rm -fr ~/.$FILE
   ln -s $BASE_DIR/$FILE ~/.$FILE
 done
+
+# homebrewの設定
+if [ `uname` == 'Darwin' ]; then
+  # Macの場合
+  which brew > /dev/null 2>&1
+  if [ $? -eq 1 ]; then
+    # homebrewのインストール
+    echo "Install: Homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  fi
+
+  # 各種ツールのインストール
+  echo "Install: Brew Each Tools"
+  sudo $BASE_DIR/brew/setup.sh
+fi
